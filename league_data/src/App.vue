4ac355dd94cc7a4a0f4989939e38b1c783a7bba8
upveo <upveo@kit.edu>
2023-02-01 14:58:36 +0100

    init: Vue-App

--- league_data/src/App.vue
+++ league_data/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LeagueData msg="MOIN Meyster"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LeagueData from "@/components/league-data";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LeagueData,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ league_data/src/components/league-data.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1 className="text-3xl font-bold underline ">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'League-Data',
+  props: {
+    msg: String
+  }
+}
+</script>
+
